1e495f9799c66a833710f45b3829d72ec26c142d
nurulislamrimon <nurulislamrimon@gmail.com>
2025-10-30 11:49:26 +0600

    CLOUDFLARE_15Y_CERTIFIED/SYSTEM_INTEGRATED: nginx/ssl will contain origin pem and key that come from cloudflare origin server - It's manual process

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 import express from "express";
-import { execSync } from "child_process";
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send(`Hello World! From ${process.env.APP_NAME}`);
+app.use((req, res, next) => {
+  const host = req.headers.host;
+  const tenant = host + " is your host, It's working!";
+  req.tenant = tenant;
+  next();
 });
 
-app.post("/add/:domain", (req, res) => {
-  const domain = req.params.domain;
-  execSync(`node /root/auto-ssl.js ${domain}`);
-  res.send(`Hello World! From ${process.env.APP_NAME}`);
+app.get("/", (req, res) => {
+  res.send({
+    success: true,
+    message: `Hello World! From ${process.env.APP_NAME}`,
+    tenant: req.tenant,
+  });
 });
+
+// app.post("/add/:domain", (req, res) => {
+//   const domain = req.params.domain;
+//   execSync(`node /root/auto-ssl.js ${domain}`);
+//   res.send(`Hello World! From ${process.env.APP_NAME}`);
+// });
 
 app.listen(port, () => {
   console.log(`${process.env.APP_NAME} running on port ${port}`);
